7a5fa1bdcd6094330ed3ced126a8202c870f6142
Kurahara0930 <kuma.kura193@gmail.com>
2025-02-27 23:03:56 +0900

    kadai_020,kadai_021_jsファイル内コード修正

--- kadai_021/event.js
+++ kadai_021/event.js
@@ -1,7 +1,7 @@
 const btn = document.getElementById('btn');
+const text = document.getElementById('text');
 
 btn.addEventListener('click', () => {
-    const text = document.getElementById('text');
     setTimeout(() => {
         text.textContent = 'ボタンをクリックしました';
     }, 2000);
